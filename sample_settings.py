ae696b42d5fe13ee3a89c15e937fe03162363eb5
avi <hi@avi.im>
2020-12-19 15:46:24 +0530

    add sample settings

---
+++ sample_settings.py
@@ -0,0 +1,3 @@
+TELEGRAM_ACCESS_TOKEN = 'Telegram bot API key goes here'
+TELEGRAPH_ACCESS_TOKEN = 'Telegraph API key goes here'
+TELEGRAM_GROUP_ID = 'The channel or group ID where you want to upload'
